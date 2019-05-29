cc6a34dab517256347850db4c63c6dbb8c965b25
Nuno Pereira <nmfpereira@gmail.com>
2019-05-29 19:00:07 +0100

    Initial Commit

---
+++ PropertyAnimatorCollection/CollectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionVC.swift
+//  PropertyAnimatorCollection
+//
+//  Created by Nuno Pereira on 29/05/2019.
+//  Copyright © 2019 Nuno Pereira. All rights reserved.
+//
+
+import UIKit
+
+class CollectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
